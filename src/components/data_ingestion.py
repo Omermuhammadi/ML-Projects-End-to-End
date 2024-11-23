e0f4e5ea3fdbbcd10adf7d40c440ab1d399889cb
Omermuhammadi <khanomer679@gmail.com>
2024-11-23 15:49:03 +0500

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.model_trainer import ModelTrainer
+
 
 
 from src.components.data_transformation import DataTransformation
@@ -51,10 +53,12 @@ class DataIngestion:
             raise customException(e,sys)
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
